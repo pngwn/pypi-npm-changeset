6116a36cf5b97561d820abd458c1c4cd67ead471
pngwn <hello@pngwn.io>
2023-07-07 14:23:42 +0100

    fixes

--- .github/actions/publish-pypi/index.ts
+++ .github/actions/publish-pypi/index.ts
@@ -1,8 +1,13 @@
 import { getInput, info } from '@actions/core'
+import { getPackagesSync } from "@manypkg/get-packages";
+import { context } from "@actions/github"
 
 function run() {
+info(JSON.stringify(context, null, 2))
+  // const { tool, packages, rootPackage, rootDir } = getPackagesSync();
+
   const user = getInput("user");
-  const passwords = getInput("packages");
+  const passwords = getInput("passwords");
   info(user)
   info(passwords);
 
